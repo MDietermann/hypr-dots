76248a3837aa5d0e7af5caab4148b35a7e2bd96b
MDietermann <mdietermann253@gmail.com>
2026-04-16 14:04:26 +0200

    nvim: open a per-pid server socket for theme-switch hook

--- hypr-base/.config/nvim/lua/config/options.lua
+++ hypr-base/.config/nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- theme-switch integration: advertise a nvim server per pid
+local sock = (vim.env.XDG_RUNTIME_DIR or "/tmp") .. "/nvim-" .. vim.fn.getpid()
+pcall(vim.fn.serverstart, sock)
