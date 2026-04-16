c5c54d4f81fe446e2b07016d12b2f3659a57d72a
MDietermann <mdietermann253@gmail.com>
2026-04-16 14:10:22 +0200

    theme-default: zsh color exports

---
+++ theme-default/.local/share/zsh/theme-colors.zsh
@@ -0,0 +1,5 @@
+# theme-default palette (consumed by prompt/plugins as they see fit)
+export THEME_NAME="default"
+export THEME_ACCENT="#33ccff"
+export THEME_BG="#1a1a1a"
+export THEME_FG="#ffffff"
